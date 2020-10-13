cae4163c46976096d9682f0684a464c36845997d
PRAY <wuwenbang@foxmail.com>
2020-10-13 14:07:44 +0800

    deploy-update

---
+++ precache-manifest.b8c848e20e9750329d0215d655ebfa14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e097842a5b0758096f16e9c44267b6b",
+    "url": "/lemon-nav-website/index.html"
+  },
+  {
+    "revision": "fb4a2a8009f38a88ee8f",
+    "url": "/lemon-nav-website/static/css/main.a851b701.chunk.css"
+  },
+  {
+    "revision": "a011116a33fa2d4d741e",
+    "url": "/lemon-nav-website/static/js/2.49818e37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lemon-nav-website/static/js/2.49818e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4a2a8009f38a88ee8f",
+    "url": "/lemon-nav-website/static/js/main.f9ffc261.chunk.js"
+  },
+  {
+    "revision": "5a7b02e3d227cbfced32",
+    "url": "/lemon-nav-website/static/js/runtime-main.fc9ec777.js"
+  },
+  {
+    "revision": "9b49099d11d403067ec784320edcedfb",
+    "url": "/lemon-nav-website/static/media/default.9b49099d.jpg"
+  },
+  {
+    "revision": "2ee2127e848aa65ccc5a08395d5594f4",
+    "url": "/lemon-nav-website/static/media/image1.2ee2127e.jpg"
+  },
+  {
+    "revision": "0e7efa42481c6138a743e87257f2e3ce",
+    "url": "/lemon-nav-website/static/media/image2.0e7efa42.jpg"
+  },
+  {
+    "revision": "543437f9bb8c916217bc5cf474a71f98",
+    "url": "/lemon-nav-website/static/media/image3.543437f9.jpg"
+  },
+  {
+    "revision": "40351d88afa27afbf0ba147008142926",
+    "url": "/lemon-nav-website/static/media/image4.40351d88.jpg"
+  }
+]);
